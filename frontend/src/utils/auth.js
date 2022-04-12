a5680572ec31f251c1ccea624d7e9913eb5b73f8
BakayAlexander <bakay.dvr@gmail.com>
2022-04-12 17:40:07 +0300

    bugfixes

--- frontend/src/utils/auth.js
+++ frontend/src/utils/auth.js
@@ -1,7 +1,5 @@
 // export const BASE_URL = 'https://auth.nomoreparties.co';
-export const BASE_URL =
-  process.env.REACT_APP_BASE_URL ||
-  'api.mesto.bakay.students.nomoredomains.work';
+export const BASE_URL = 'api.mesto.bakay.students.nomoredomains.work';
 
 const prepareDate = (res) => {
   if (res.ok) {
