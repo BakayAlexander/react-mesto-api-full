23237f486690e9d4ee9c635c0ccf63256a7c5b6c
BakayAlexander <bakay.dvr@gmail.com>
2022-04-12 12:43:43 +0300

    bugfixes

--- backend/app.js
+++ backend/app.js
@@ -39,7 +39,7 @@ app.use(routes);
 app.use((req, res, next) => {
   next(new NotFoundError('Запрашиваемый ресур не найден'));
 });
-app.use(errorLogger); // подключаем логгер ошибок
+// app.use(errorLogger); // подключаем логгер ошибок
 app.use(errors());
 app.use(errorHandler);
 
