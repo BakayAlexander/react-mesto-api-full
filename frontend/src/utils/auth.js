5e51c0b29689063c3100f99d729f7b5366fa3c51
BakayAlexander <bakay.dvr@gmail.com>
2022-04-13 14:37:26 +0300

    installing cors localy

--- frontend/src/utils/auth.js
+++ frontend/src/utils/auth.js
@@ -1,4 +1,4 @@
-export const BASE_URL = 'https://api.mesto.bakay.students.nomoredomains.work';
+export const BASE_URL = 'http://api.mesto.bakay.students.nomoredomains.work';
 // export const BASE_URL = 'http://localhost:5000/';
 
 const prepareDate = (res) => {
